3960ec381b54ff66d5a6e51f2d5b46a680cf8f35
DiegoSValderrama <105023447+DiegoSValderrama@users.noreply.github.com>
2023-09-09 21:15:33 -0500

    Mi primer commit

---
+++ TAREA1_SEMANA2_TANAKA/TAREA1_SEMANA2_TANAKA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA1_SEMANA2_TANAKA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
